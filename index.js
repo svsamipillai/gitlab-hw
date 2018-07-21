7cb13195ab0cf2f12e5ba68a2db401a0397214f2
Socratees V Samipillai <ssamipillai@us.ibm.com>
2018-07-21 16:25:25 -0500

    Setting up a db route

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl:true
+});
+
+
 var app = express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -17,6 +24,18 @@ app.get('/times', (req, res) => {
     result += i + ' '
   }
   res.send(result)
+});
+
+app.get('/db', async(req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    res.render('pages/db', result);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
 });
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
